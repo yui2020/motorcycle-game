a00cce73b492eccaf107993d6689a1b81100040a
YuiFujita <kuota0204@gmail.com>
2020-05-24 01:00:27 +0900

    Add game functions

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var ctx = c.getContext("2d");
 c.width = 500;
 c.height = 350;
 
+var size = 15;
+
 document.body.appendChild(c);
 
 var perm = [];
 
 while (perm.length < 255) {
   while (perm.includes(val = Math.floor(Math.random() * 255)));
-    perm.push(val);
-  }
+  perm.push(val);
+}
 
 var lerp = (a, b, t) => a + (b - a) * (1 - Math.cos(t * Math.PI)) / 2;
 
@@ -22,23 +24,70 @@ var noise = x => {
 var player = new function() {
   this.x = c.width / 2;
   this.y = 0;
+  this.ySpeed = 0;
   this.rot = 0;
+  this.rSpeed = 0;
 
   this.img = new Image();
   this.img.src = "images/moto.png";
 
   this.draw = function() {
-    ctx.drawImage(this.img, this.x, 100);
+    var p1 = c.height - noise(t + this.x) * 0.25;
+    var p2 = c.height - noise(t + 5 + this.x) * 0.25;
+  
+    var grounded = 0;
+
+    if(p1 - size > this.y) {
+      this.ySpeed += 0.1;
+    } else {
+      this.ySpeed -= this.y - (p1 - size);
+      this.y = p1 - size;
+
+      grounded = 1;
+    }
+
+    if(!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5) {
+      playing = false;
+      this.rSpeed = 5;
+      k.ArrowUp = 1;
+      this.x -= speed * 5;
+    }
+
+    var angle = Math.atan2((p2 - size) - this.y, (this.x + 5) - this.x);
+
+    this.y += this.ySpeed;
+
+    if(grounded && playing) {
+      this.rot -= (this.rot - angle) * 0.5;
+      this.rSpeed = this.rSpeed - (angle - this.rot);
+    }
+
+    this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
+    this.rot -= this.rSpeed * 0.1;
+  
+    if(this.rot > Math.PI) this.rot = -Math.PI;
+    if(this.rot < -Math.PI) this.rot = Math.PI;
+
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.rot);
+    ctx.drawImage(this.img, -size, -size, 30, 30);
+
+    ctx.restore();
   }
 }
 
 var t = 0;
-function loop() {
+var speed = 0;
+var playing = true;
+var k = {ArrowUp: 0, ArrowDown: 0, ArrowLeft: 0, ArrowRight: 0};
 
-  t += 1;
+function loop() {
+  speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.1;
+  t += 10 * speed;
   ctx.fillStyle = "#19f";
   ctx.fillRect(0, 0, c.width, c.height);
-  
+
   ctx.fillStyle = "black";
 
   ctx.beginPath();
@@ -51,7 +100,12 @@ function loop() {
   ctx.lineTo(c.width, c.height);
 
   ctx.fill();
+
+  player.draw();
   requestAnimationFrame(loop);
 }
+
+onkeydown = d => k[d.key] = 1;
+onkeyup = d => k[d.key] = 0;
 
 loop();
